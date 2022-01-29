4f6aa33282dd37b06e563e8d13e1566e538c923b
toast-sys <happybiau@gmail.com>
2022-01-29 12:59:33 +0100

    feat : ajout chrono

--- js/fonctions.js
+++ js/fonctions.js
@@ -8,5 +8,36 @@ $('document').ready(function(){
 	
 	$('.prixMax').html(prixMax);
 	
+	$('#commencer').click(function() {
+		
+		isPlay = true;
+		secondes = 30;
+		$('.chrono').html(secondes);
+		$('.secondes').html('Secondes');
+		prix = Math.floor(Math.random() * prixMax);
+		$('#paris').css({'visibility': 'visible', 'opacity': '1'});
+		setTimeout(function() {$('#nombre').focus()}, 100);
+		$('#nombre').val('');
+		
+		var chrono = setInterval(function(){
+		if (secondes == 0) {
+			isPlay = false;
+			clearInterval(chrono);
+			$('.reponse').html('Perdu !');
+			$('#paris').css({'visibility': 'hidden', 'opacity': '0'});
+		}
+		else if (isPlay) {
+			secondes--;
+			$('.chrono').html(secondes);
+			if (secondes == 1) {
+				$('.secondes').html('Seconde');
+			}
+		}
+		else {
+			clearInterval(chrono);
+			$('#paris').css({'visibility': 'hidden', 'opacity': '0'});
+		}
+		}, 1000);
+	});
 });
 
