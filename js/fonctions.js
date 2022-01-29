07bea817a06a87c1fad386121cf6b139ab9bcf67
Axel <axel.kergus22@gmail.com>
2022-01-29 12:58:45 +0100

    feat: ajout pari

--- js/fonctions.js
+++ js/fonctions.js
@@ -8,5 +8,24 @@ $('document').ready(function(){
 	
 	$('.prixMax').html(prixMax);
 	
+	$('#paris').submit(function(event) {
+		if (isPlay) {
+		paris = $('#nombre').val();
+		
+		if (paris == prix) {
+			isPlay = false;
+			$('.reponse').html('Gagné !');
+		}
+		else if (paris < prix) {
+			$('.reponse').html('Plus grand !');
+		}
+		else {
+			$('.reponse').html('Plus petit !');
+		}
+		
+		$('#nombre').val('');
+		}
+		event.preventDefault();
+	});
 });
 
